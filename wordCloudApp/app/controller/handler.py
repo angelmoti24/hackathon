9d0ce39c9ed20a418821e277baab1ba780c43bfb
Angel Franco <angel@insaite.io>
2020-10-18 04:24:50 -0500

    Twitter clouds results added

--- wordCloudApp/app/controller/handler.py
+++ wordCloudApp/app/controller/handler.py
@@ -6,19 +6,10 @@ import cv2
 #from PIL import Image
 from wordcloud import WordCloud, ImageColorGenerator
 from nltk.corpus import stopwords as sw
-from os import environ
 from flask_restful import Resource
 from json import JSONEncoder, loads
 from flask import request, make_response
 
-
-
-
-class JsonEncoder(JSONEncoder):
-    def default(self, object):
-        if isinstance(object, ObjectId):
-            return str(object)
-        return JSONEncoder.default(self, object)
 
 
 class WordCloud(Resource):
@@ -31,14 +22,14 @@ class WordCloud(Resource):
             response = {
                 "imageBase64": base64.b64encode(image.to_array()),
             }
-            return make_response(response, 200)
+            return make_response(json.dumps(response), 200)
         except Exception as e:
             response = {'error':str(e)}
-            logger.error(f"WordCloud task could not be executed: {str(e)}")
             return make_response(response, 400)
 
 
 def clean_text(text):
+    stopwords = sw.words("spanish")+ ["rt","https","xaguzar2","bbva","hola","peruano","año","mex","anos","vibqpmlgsg"]
     cleaned_text = unicodedata.normalize('NFD', text).encode('ascii', 'ignore')
     cleaned_text = re.sub('[^a-zA-Z ]', " ", cleaned_text.decode("utf-8"), flags=re.UNICODE)
     text = u' '.join(cleaned_text.lower().split())
@@ -51,13 +42,19 @@ def clean_text(text):
 
 
 def wordcloud(dataset):
-    stopwords = sw.words("spanish")+ ["rt","https","xaguzar2","bbva","hola","peruano","año","mex","anos","vibqpmlgsg"]
-    data=pd.read_pickle(f"datasets/{dataset}.pkl")
+    data=pd.read_csv(f"datasets/{dataset}.csv")
     data["cleanText"] = data.text.map(clean_text)
     imagen_act=cv2.imread(f"masks/{dataset}.png")
+    text_cloud = " ".join(data.cleanText.tolist())
+    if dataset=="instagram":
+        cmap = "RdPu"
+    elif dataset=="twitter":
+        cmap = "blues"
+    elif dataset == "maps":
+        cmap = "BuGn"
     wordcloud = WordCloud(mask=imagen_act,
                         max_font_size=200,
                         background_color="#393d3f",
-                        colormap="Blues"
+                        colormap=cmap
                         ).generate(text_cloud)
     return wordcloud
